1c565d964229001d9275377bfe4c011506d05497
WebTechStreet5 <webtechstreet4@WebTechStreets-SRK.local>
2019-10-15 16:58:11 +0530

    24-sticky_nav.html

---
+++ stripe_follow_along_nav.js
@@ -0,0 +1,23 @@
+const triggers = document.querySelectorAll('.cool > li');
+const background = document.querySelector('dropdownBackground');
+const nav = document.querySelector('.top');
+
+function handleEnter(){
+  this.classList.add('trigger-enter');
+      setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150);
+      background.classList.add('open');
+
+
+const dropdown = this.querySelector('.dropdown');
+console.log(dropdown);
+}
+
+
+
+function handleLeave() {
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
+  }
+
+triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
